b978f7b1dfadb72a38e81692a89e1f09328f5240
Shakhana <shamilova.08@gmail.com>
2022-09-22 22:33:11 +0300

    Initial commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,7 +3,6 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
     @Test
@@ -18,8 +17,5 @@ class MobileBankApiTestV1 {
                 // Проверки
                 .then()
                 .statusCode(200);
-        // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
-        .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-        ;
     }
 }
